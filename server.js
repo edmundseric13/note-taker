cf2468517e2edae34e64af65a84796e14ef8594d
Eric Edmunds <ecedmunds08@gmail.com>
2024-05-16 14:36:53 -0600

    added server.js file plus misc changes

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const path = require('path');
+const fs = require('fs');
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./develop/db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post('/api/notes', (req, res) => {
+  fs.readFile('./develop/db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    let notes = JSON.parse(data);
+    let newNote = req.body;
+    newNote.id = notes.length + 1;
+    notes.push(newNote);
+    fs.writeFile('./develop/db/db.json', JSON.stringify(notes), (err) => {
+      if (err) throw err;
+      res.json(newNote);
+    });
+  });
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './notes.html'));
+  });
+
+app.delete('/api/notes/:id', (req, res) => {
+  fs.readFile('./develop/db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    let notes = JSON.parse(data);
+    notes = notes.filter(note => note.id != req.params.id);
+    fs.writeFile('./develop/db/db.json', JSON.stringify(notes), (err) => {
+      if (err) throw err;
+      res.json({ message: `Note with id ${req.params.id} deleted` });
+    });
+  });
+});
